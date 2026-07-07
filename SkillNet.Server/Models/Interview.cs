f4cafc8d3fcbcb187a65323e2ea4c51d4fcf0f6e
Dinuri2004 <dinuri.chamindi04@gmail.com>
2026-07-08 01:48:31 +0530

    Add interview models and DTOs

---
+++ SkillNet.Server/Models/Interview.cs
@@ -0,0 +1,25 @@
+﻿namespace SkillNet.Server.Models
+{
+    public class Interview
+    {
+        public int InterviewId { get; set; }
+
+        public int ApplicationId { get; set; }
+
+        public string? InterviewType { get; set; }
+
+        public int InterviewRound { get; set; }
+
+        public DateTime ScheduledDate { get; set; }
+
+        public int Duration { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? MeetingLink { get; set; }
+
+        public string? Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
