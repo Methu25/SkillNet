2d460babf71a29663aaf63ebb3f1594fc7fd886e
Dinuri2004 <dinuri.chamindi04@gmail.com>
2026-07-15 13:48:13 +0530

    Add EF Core migration for interview module tables

--- SkillNet.Domain/Entities/InterviewAssignment.cs
+++ SkillNet.Domain/Entities/InterviewAssignment.cs
@@ -3,7 +3,13 @@ namespace SkillNet.Domain.Entities
     public class InterviewAssignment
     {
         public int InterviewId { get; set; }
+
         public int InterviewerId { get; set; }
+
         public string? Role { get; set; }
+
+        public Interview? Interview { get; set; }
+
+        public Interviewer? Interviewer { get; set; }
     }
 }
